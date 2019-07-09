d6b811332a74162f8f6c6a203a3acd8af4846b16
Adnan <adnan.chowdhury@outlook.com>
2019-07-09 18:07:55 -0500

    Added info about logic of rocket going up

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,5 +1,5 @@
 //Adding event listeners to each of the rockets aka blocks
-let clickCount = 0;
+let clickCount = 0; //clickCount alter the order property so it whichever rocket is clicked even if it has been clicked before will always go to top.
 
 
 const rocketRed = document.querySelector('.block--red');
@@ -24,6 +24,8 @@ const rocketGray = document.querySelector('.block--gray');
 
 rocketGray.addEventListener('click', () => moveRocketUp(rocketGray));
 
+
+//This function sets the order which causes it to move up by altering it order via flex-box
 function moveRocketUp(target){
 
     target.style.order = '-1' + clickCount;
